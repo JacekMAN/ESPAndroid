c4be327160ec1ca74e699eef9c6ad4b6ec68ad90
JacekMAN <Jacek24734>
2017-01-07 14:28:31 +0100

    Pierwszy Komit

---
+++ paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/ReadMessage.java
@@ -0,0 +1,26 @@
+package paho.mqtt.java.example;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+/**
+ * Created by DOM on 07.01.2017.
+ */
+
+public class ReadMessage implements MqttCallback {
+    @Override
+    public void connectionLost(Throwable cause) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
+    }
+}
