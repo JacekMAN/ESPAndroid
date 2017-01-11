edefd5ebadaa896487d187d32b228ba82a9409cf
JacekMAN <Jacek24734>
2017-01-11 12:31:32 +0100

    Zmiany

--- paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/PahoExampleActivity.java
+++ paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/PahoExampleActivity.java
@@ -13,6 +13,8 @@
  */
 package paho.mqtt.java.example;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,8 +25,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
@@ -39,34 +47,43 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.util.ArrayList;
 
-public class PahoExampleActivity extends AppCompatActivity{
+public class PahoExampleActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
 
 
     MqttAndroidClient mqttAndroidClient;
 
-    final String serverUri = "tcp://192.168.1.59:1883";
+    final String serverUri = "tcp://jacman.ddns.info:1883";
 
-    final String clientId = "Android";
+    final String clientId = "Android7";
     final String subscriptionTopic = "#";
 
     int w1 = 0;
+    int wynik ; // obliczenie procentowe wody
+    ProgressBar woda;
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
 
-        ToggleButton button = (ToggleButton)findViewById(R.id.toggleButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                publishMessage("S2/w1",""+w1);
-            }
-        });
+        woda=(ProgressBar)findViewById(R.id.progressBar1);
 
-
+     //   ToggleButton button = (ToggleButton) findViewById(R.id.toggleButton);
+    //    button.setOnClickListener(new View.OnClickListener() {
+     //       @Override
+     //       public void onClick(View view) {
+     //           publishMessage("S2/w1", "" + w1);
+     //       }
+     //   });
 
 
         mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
@@ -90,7 +107,7 @@ public class PahoExampleActivity extends AppCompatActivity{
 
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
-                dosalemWiadomosc(topic,new String(message.getPayload()));
+                dosalemWiadomosc(topic, new String(message.getPayload()));
             }
 
             @Override
@@ -104,9 +121,6 @@ public class PahoExampleActivity extends AppCompatActivity{
         mqttConnectOptions.setCleanSession(false);
         mqttConnectOptions.setUserName("Jacek");
         mqttConnectOptions.setPassword("24734396567".toCharArray());
-
-
-
 
 
         try {
@@ -130,38 +144,159 @@ public class PahoExampleActivity extends AppCompatActivity{
             });
 
 
-        } catch (MqttException ex){
+        } catch (MqttException ex) {
             ex.printStackTrace();
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     //Wyswietlanie wiadomosci
-    private void dosalemWiadomosc(String topic,String message){
-        switch (topic){
-            case "S1/dane0" :
-                TextView temp1 = (TextView) findViewById(R.id.textView2);
-                temp1.setText("Temp 1: " + message);
-                break;
-            case "S1/dane1" :
-                TextView temp2 = (TextView) findViewById(R.id.textView3);
-                temp2.setText("Temp 2: " + message);
-                break;
-            case "S2/w1" :
-                ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
-                if(message=="1"){
-                    toggleButton.setChecked(true);
-                }else{
-                    toggleButton.setChecked(false);
+    private void dosalemWiadomosc(String topic, String message) {
+        switch (topic) {
+            case "S1/dane5":
+                TextView temp5 = (TextView) findViewById(R.id.textView2);
+                temp5.setText(message + "`C");
+                double zmienna5 = new Double(message);
+                temp5.setTextColor(Color.rgb(50,100,200));
+                if (zmienna5 >= 1) {
+                    temp5.setTextColor(Color.YELLOW);
                 }
-                w1 = w1==1?0:1;
+                if (zmienna5 >= 10) {
+                    temp5.setTextColor(Color.GREEN);
+                }
+
+
                 break;
+            case "S1/dane7":
+
+                TextView temp7 = (TextView) findViewById(R.id.textView3);
+
+                temp7.setText(message + "`C");
+                double zmienna2 = new Double(message);
+                temp7.setTextColor(Color.RED);
+                if (zmienna2 >= 15) {
+                    temp7.setTextColor(Color.YELLOW);
+                }
+                if (zmienna2 >= 20) {
+                    temp7.setTextColor(Color.GREEN);
+                }
+                break;
+            case "S1/dane8":
+                TextView temp8 = (TextView) findViewById(R.id.textView6);
+
+                temp8.setText(message + "`C");
+                double zmienna8 = new Double(message);
+                temp8.setTextColor(Color.RED);
+                if (zmienna8 >= 15) {
+                    temp8.setTextColor(Color.YELLOW);
+                }
+                if (zmienna8 >= 20) {
+                    temp8.setTextColor(Color.GREEN);
+                }
+
+                break;
+
+            case "S1/dane0":
+                TextView temp0 = (TextView) findViewById(R.id.textView12);
+                temp0.setText(message + "`C");
+                double zmienna0 = new Double(message);
+                temp0.setTextColor(Color.YELLOW);
+                if (zmienna0 >= 30) {
+                    temp0.setTextColor(Color.GREEN);
+                }
+                if (zmienna0 >= 60) {
+                    temp0.setTextColor(Color.RED);
+                }
+
+
+                break;
+
+            case "S1/dane1":                                                 // woda czujnik 1
+                TextView temp1 = (TextView) findViewById(R.id.textView14);
+                temp1.setText(message + "`C");
+                double zmienna11 = new Double(message);
+
+                temp1.setTextColor(Color.YELLOW);
+                if (zmienna11 >= 45) {
+                    temp1.setTextColor(Color.GREEN);
+                }
+                zmienna11 = zmienna11 - 35;
+                zmienna11 = zmienna11 * 4;
+                if (zmienna11 >= 100) zmienna11=100;
+                if(zmienna11 <= 0 ) zmienna11=0;
+
+
+                wynik = (int)zmienna11;
+
+
+                break;
+
+            case "S1/dane2":                                                 // woda czujnik 2
+                TextView temp2 = (TextView) findViewById(R.id.textView14);
+                double zmienna12 = new Double(message);
+                zmienna12 = zmienna12 - 35;
+                zmienna12 = zmienna12 * 4;
+                if (zmienna12 >= 100) zmienna12=100;
+                if(zmienna12 <= 0 ) zmienna12=0;
+
+                int wynik2 = (int)zmienna12;
+                wynik= wynik+wynik2;
+                wynik=wynik/2;
+                
+                woda.setProgress(wynik);
+                String procent = Integer.toString(wynik);
+                TextView temp22 = (TextView) findViewById(R.id.textView15);
+                temp22.setText(procent+"%");
+
+
+                break;
+
+
+
+            // Obliczenie stanu wody
+
+
+
+
+            case "S1/dane9":
+                TextView temp9 = (TextView) findViewById(R.id.textView9);
+                temp9.setText(message + "`C");
+                double zmienna9 = new Double(message);
+
+                if (zmienna9 <= 0) {
+                    temp9.setTextColor(Color.BLUE);
+                }
+                break;
+            case "S1/dane6":
+                TextView temp6 = (TextView) findViewById(R.id.textView10);
+                temp6.setText(message + "`C");
+                double zmienna6 = new Double(message);
+
+                if (zmienna6 <= 0) {
+                    temp6.setTextColor(Color.rgb(50,100,200));
+                }
+                break;
+
+
+         //   case "S2/w1":
+         //       ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+         //       if (message == "1") {
+         //           toggleButton.setChecked(true);
+         //       } else {
+         //           toggleButton.setChecked(false);
+         //       }
+         //       w1 = w1 == 1 ? 0 : 1;
+          //      break;
         }
     }
-    private void addToHistory(String mainText){
+
+    private void addToHistory(String mainText) {
         System.out.println("LOG: " + mainText);
-       // mAdapter.add(mainText);
+        // mAdapter.add(mainText);
         Snackbar.make(findViewById(android.R.id.content), mainText, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
 
@@ -186,18 +321,19 @@ public class PahoExampleActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
-    public void subscribeToTopic(){
+    public void subscribeToTopic() {
         try {
             mqttAndroidClient.subscribe(subscriptionTopic, 1, null, new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    addToHistory("Subscribed!");
-                }
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            addToHistory("Połączono z serwerem");
+                        }
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    addToHistory("Failed to subscribe");
-                }}
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            addToHistory("Failed to subscribe");
+                        }
+                    }
 //            ,new IMqttMessageListener() {
 //                        @Override
 //                        public void messageArrived(String topic, MqttMessage message) throws Exception {
@@ -217,26 +353,61 @@ public class PahoExampleActivity extends AppCompatActivity{
 //                }
 //            });
 
-        } catch (MqttException ex){
+        } catch (MqttException ex) {
             System.err.println("Exception whilst subscribing");
             ex.printStackTrace();
         }
     }
 
-    public void publishMessage(String publishTopic,String publishMessage){
+    public void publishMessage(String publishTopic, String publishMessage) {
 
         try {
             MqttMessage message = new MqttMessage();
             message.setPayload(publishMessage.getBytes());
             mqttAndroidClient.publish(publishTopic, message);
-            addToHistory("Message Published");
-            if(!mqttAndroidClient.isConnected()){
-                addToHistory(mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            addToHistory("wiadomość Wysłana");
+            if (!mqttAndroidClient.isConnected()) {
+                addToHistory(mqttAndroidClient.getBufferedMessageCount() + " wiadomość  zbuforowana");
             }
         } catch (MqttException e) {
-            System.err.println("Error Publishing: " + e.getMessage());
+            System.err.println("Błąd wysyłania: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("PahoExample Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
